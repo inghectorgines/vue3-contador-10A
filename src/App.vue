d00a4ddaccb15dcc1574e1e1302bf3f9470606cc
inghectorgines <ing.hectorgines@live.com>
2022-09-09 15:30:43 -0500

    Vue3 contador 10A

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script>
+  export  default {
+    data: () => ({
+      contador : 10,
+    }),
+    methods: {
+      saludar(){
+        console.log("Hola 10A | @");
+      },
+      incrementar() {
+        this.contador++;
+      },
+      decrementar() {
+        this.contador--;
+      },
+      resetear(){
+        this.contador = 10;
+      },
+    }
+  };
+</script>
+
+<template>  
+  <img src="./assets/logo.svg" alt="Logo VUE" width="125" height="125">
+  <div class="container">
+    <small>
+      <h1>Hola desde VUE.js 3</h1>
+    </small>
+    <hr>
+    <h2>Contador {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="resetear()">Reset</button>
+  </div>
+</template> 
+
+<style scoped>
+  .container{
+    text-align: center;
+  }
+  small{
+    color: #666;
+  }
+  h1 {
+    font-weight: bolder;
+  }
+  h2{
+    font-size: 2.5rem;
+  }
+  button {
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0px 8px;
+  }
+</style>
